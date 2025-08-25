7345f5e904129bfc8070d648a8545472284def8d
tje0714 <tje0714@outlook.kr>
2025-08-25 12:27:39 +0900

    java start

---
+++ src/VariableEx1.java
@@ -0,0 +1,11 @@
+public class VariableEx1 {
+
+    public  static void main(String[] args){
+
+        // 변수 : 메모리에 값을 저장하기 위한 공간의 명칭
+        // 이 때, 저장되는 값이 변할 수 있기 때문에 변수
+
+        // 자료형 (data Type)
+        // - 변수 또는 값의 크기와 형식
+    }
+}
