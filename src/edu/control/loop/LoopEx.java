fa7a85f091165ac9836249499448e298115c2aa9
tje0714 <tje0714@outlook.kr>
2025-08-26 10:54:32 +0900

    Loop for 조건문 / charAt(index) / length() 메서드

--- src/edu/control/loop/LoopEx.java
+++ src/edu/control/loop/LoopEx.java
@@ -1,11 +1,11 @@
-package edu.control.loof;
+package edu.control.loop;
 
 import java.util.Scanner;
 
 /**
  * 루프 기능 설정용 클래스
  */
-public class LoofEx { // <html> 과 같이 페이지 내 코드 시작을 나타내는 구문
+public class LoopEx { // <html> 과 같이 페이지 내 코드 시작을 나타내는 구문
 
     /**
      * 1부터 10까지 출력하기
@@ -81,6 +81,23 @@ public class LoofEx { // <html> 과 같이 페이지 내 코드 시작을 나타
      * 변수이름 (int 최소    int 최대    int i)
      * method4() 완성하기
      */
+    public void method4() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("숫자 두가지를 입력하세요 : ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int min = Math.min(a, b);
+        int max = Math.max(a, b);
+
+            for (int i = min; i <= max; i++ ){
+                System.out.println(i);
+            }
+
+
+    }
+
 } // <html> 과 같이 페이지 내 코드 종료를 나타내는 구문
 
 /** 주의할 점
