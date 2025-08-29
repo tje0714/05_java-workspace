a3e09c5b1e8bb03b438fc6f7a68b3e0032d4dd2f
tje0714 <tje0714@outlook.kr>
2025-08-29 12:10:18 +0900

    메서드 기능 과 화면 분류 후 실습

--- src/edu/oop/method/StudentService.java
+++ src/edu/oop/method/StudentService.java
@@ -42,7 +42,7 @@ public class StudentService {
             input = sc.nextInt(); // input type = number 대신 사용
 
             switch(input){
-                case 1:
+                case 1: // register.html 에서 학생 등록하는 태그를 작성
                     System.out.println("학생을 등록할 변수를 선택하세요.");
                     System.out.print("1=std1 / 2=std2   :");
                     int select = sc.nextInt();
@@ -53,9 +53,10 @@ public class StudentService {
                         System.out.println("학생 등록을 시작하겠습니다.");
                         // 학생 등록을 위한 메서드 호출
                         std2 = createStudent();
+                        studentInform(std2); // 등록이 잘 되었는지 학생 정보 확인
                     }
                     break;
-                case 2:
+                case 2: // mypage.html
                     System.out.println("학생 정보를 확인합니다.");
                     System.out.println("학생 정보를 확인할 번호를 선택하세요.");
                     System.out.print("1=std1 / 2=std2   :");
@@ -72,19 +73,50 @@ public class StudentService {
 //                        System.out.println("학생학번 "  +  std2.getStudentNumber());
 //                        System.out.println("학생성별 "  +  std2.getGender());
                     break;
+                case 3: //mypage.html 수정하는 작업이 일어나는 상황
+                    System.out.println("개명하셨군요! 이름을 수정하세요.");
+                    System.out.print("이름을 수정할 학생 선택 (1=std1 / 2=std2)   :");
+                    select = sc.nextInt();
+
+                    if(select == 1) {
+                        updateStudentName(std1); // 회원정보 수정 진행
+                        studentInform(std1);     // 수정된 결과 조회
+                    } else {
+                        updateStudentName(std2); // 회원정보 수정 진행
+                        studentInform(std2);     // 수정된 결과 조회
+                    }
+                    break;
+                case 4: // 관리자 페이지 - java 역량 tab
+                    System.out.println("Java 역량을 수정하는 공간입니다.");
+
+                    // html 기준 search 검색하는 input 창 placeholder 로 검색어 입력 예시 작성되어 있을 것
+                    System.out.print("Java 역량을 수정할 학생 선택 (1=std1 / 2=std2)   :");
+                    select = sc.nextInt();
+                    if(select == 1) {
+                        updateJava(std1);
+                        studentInform(std1); //실제 html 에서는 새로고침과 같은 기능을 사용
+                    } else {
+                        updateJava(std2);
+                        studentInform(std2);
+                    }
+                    break;
                 case 0: System.out.println("프로그램을 종료합니다."); return;
                 default: System.out.println("잘못된 번호를 선택하셨습니다.");
             }
         }
     }
 
-
+    /**
+     *
+     * @return
+     */
     private Student createStudent() {
         System.out.print("이름 :" );
-
+        String createName =  sc.next();
         System.out.print("학번 :" );
-
+        String createNumber =  sc.next();
         System.out.print("성별(남/여) :" );
+        char createGender = sc.next().charAt(0);
         // sc.next().charAt(0);  sc.next()로 입력받은 문자열 중에서
         // 0 번째 인덱스 번째 문자 하나를 반환 받아 createGender 변수에 저장
 
@@ -113,7 +145,53 @@ public class StudentService {
         return  a;
     }
 
+    /**
+     * 매개 변수로 전달 받은 학생 객체의 이름 수정
+     * @param student : std1 또는 std2 (학생의 대한 정보가 담겨있는 객체 형태 위치 주소를 가져오기)
+     *
+     * 수정이나 삭제 작업은 void 형태 진행하며 수정과 삭제에 대한 결과
+     * 수정이 완료되었다면 studentInfo 에서 수정된 내용이 조회될 것이고,
+     * 수정에 실패하였다면 studentInfo 수정되기 전의 내용이 조회될 것이다.
+     */
+    private void updateStudentName(Student student){
+        System.out.print("수정해서 넣어줄 새로운 학생의 이름을 입력하세요 : ");
+        String newName =  sc.next();
 
+        // 새이름 세팅
+        student.setName(newName);
+
+
+        System.out.println("학생 이름이 수정되었습니다.");
+    }
+
+    /**
+     * Java 역량이 얼마나 증가 감소했는지 정수로 입력받고
+     * 학생의 Java 역량을 관리자가 수정
+     * 수정된 Java 역량은 최대값, 최소값의 범위를 넘어가지 못하게 if 문으로 설정
+     * @param student : std1 또는 std2 학생의 정보가 담겨있는 상자의 위치 가져오기
+     */
+    private void updateJava(Student student){
+
+        System.out.print("증가 또는 감소한 Java의 역량을 입력하세요 : ");
+        int newJava =  sc.nextInt();
+        int result = newJava + student.getJava(); // -숫자값을 입력하면 자동으로 +- 에서 -로 처리
+
+        // 새로운 점수가 유효한 범위를 벗어나는 경우
+        while(result >Student.MAX_VALUE || result < Student.MIN_VALUE){
+            System.out.println("점수는 " + Student.MIN_VALUE + " ~ " + Student.MAX_VALUE +" 사이만 가능합니다.");
+
+            System.out.println("현재 점수 : " + student.getJava() + " / 최종결과 : " + result);
+
+            System.out.println("다시 입력해주세요 : ");
+            newJava = sc.nextInt();
+            result = newJava + student.getJava();
+            // 입력 종료를 원하시면 000 를 작성해주세요.  000 숫자를 입력하면 return;
+        }
+
+
+        //  result 를 setJava에 저장
+        student.setJava(result);
+    }
 }
 
 
