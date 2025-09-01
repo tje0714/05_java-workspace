ce9b57f3588d71c2f8cf806315ebc2ca2ead1bc4
tje0714 <tje0714@outlook.kr>
2025-09-01 17:42:30 +0900

    상속 실습 - day6

--- src/edu/practice/day6/NaverShopping.java
+++ src/edu/practice/day6/NaverShopping.java
@@ -1,25 +1,7 @@
 package edu.practice.day6;
 
 public  class NaverShopping {
-        private String serviceName;
-        private String userId;
-        private String userNickname;
-        private int userLevel;
 
-        public NaverShopping(String serviceName, String userId, String userNickname, int userLevel) {
-            this.serviceName = serviceName;
-            this.userId = userId;
-            this.userNickname = userNickname;
-            this.userLevel = userLevel;
-        }
-
-        public void login() {
-            System.out.println(userNickname + "님이 " + serviceName + "에 로그인했습니다.");
-        }
-
-        public void logout() {
-            System.out.println(userNickname + "님이 " + serviceName + "에서 로그아웃했습니다.");
-        }
 
         public void search() {
             System.out.println("쇼핑몰에서 상품을 검색합니다.");
@@ -29,14 +11,6 @@ public  class NaverShopping {
             System.out.println("상품 리뷰를 작성합니다.");
         }
 
-        public String getServiceName() { return serviceName; }
-        public String getUserId() { return userId; }
-        public String getUserNickname() { return userNickname; }
-        public int getUserLevel() { return userLevel; }
-        public void setServiceName(String serviceName) { this.serviceName = serviceName; }
-        public void setUserId(String userId) { this.userId = userId; }
-        public void setUserNickname(String userNickname) { this.userNickname = userNickname; }
-        public void setUserLevel(int userLevel) { this.userLevel = userLevel; }
-    }
+
 
 }
