09284c32cb724e8a98950c81c036441ba427b805
tje0714 <tje0714@outlook.kr>
2025-09-02 17:26:40 +0900

    상속 - 다형성, 추상화

---
+++ src/edu/practice/day7/model/KakaoMap.java
@@ -0,0 +1,4 @@
+package edu.practice.day7.model;
+
+public class KakaoMap {
+}
---
+++ src/edu/practice/day7/model/KakaoService.java
@@ -0,0 +1,9 @@
+package edu.practice.day7.model;
+
+/**
+ * 부모 클래스 이지만
+ * 부모 클래스를 상속받은
+ * 자식 클래스들 이 반드시 작성해야하는 기능이 포함되어 있는 클래스
+ */
+public abstract class KakaoService {
+}
