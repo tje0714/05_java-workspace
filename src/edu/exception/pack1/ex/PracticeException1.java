b61ffee5f008c804c5c550488d916561b290c78b
tje0714 <tje0714@outlook.kr>
2025-09-05 12:20:12 +0900

    Exception

--- src/edu/exception/pack1/ex/PracticeException1.java
+++ src/edu/exception/pack1/ex/PracticeException1.java
@@ -1,5 +1,6 @@
 package edu.exception.pack1.ex;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class PracticeException1 {
@@ -9,8 +10,10 @@ public class PracticeException1 {
     public void method1(){
         Scanner sc=new Scanner(System.in);
         System.out.println("두 수를 입력받아 나눈 몫 구하기");
+
         System.out.print("정수 입력 1 : ");
         int a=sc.nextInt();
+
         System.out.print("정수 입력 2 : ");
         int b=sc.nextInt();
 
@@ -22,7 +25,13 @@ public class PracticeException1 {
             System.out.println("b의 값 : " + b);
             System.out.println("0으로 숫자를 나눌 수 없습니다.");
             // 다른 방법을 시도하시겠습니까? + 나 * 와 같은 행동을 유도할 수 있음
-        } finally {
+        } catch (InputMismatchException e) {
+            System.out.println("숫자 데이터만 가능합니다.");
+        } catch (Exception e) {
+            System.out.println("개발자가 예기치 못한 문제가 발생했습니다.");
+            System.out.println("회사에 문제 보고서 전달하기 YES / NO");
+        }
+        finally {
             System.out.println("프로그램을 종료합니다.");
         }
     }
