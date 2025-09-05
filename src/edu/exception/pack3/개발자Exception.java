49cb99d49514e4fb15213188732f58b7c5377d6e
tje0714 <tje0714@outlook.kr>
2025-09-05 16:12:22 +0900

    Exception

---
+++ src/edu/exception/pack3/개발자Exception.java
@@ -0,0 +1,12 @@
+package edu.exception.pack3;
+
+public class 개발자Exception  extends RuntimeException{
+
+    public 개발자Exception() {
+        super("100 을 초과하였습니다.");
+    }
+
+    public 개발자Exception(String message) {
+        super(message);
+    }
+}
