88cf4561b31c6480b195815c8a68b574157dcc09
tje0714 <tje0714@outlook.kr>
2025-09-08 13:20:39 +0900

    IO(입출력)

--- src/edu/io/pack2/run/파일작성Run.java
+++ src/edu/io/pack2/run/파일작성Run.java
@@ -5,6 +5,7 @@ import edu.io.pack2.service.파일작성Service;
 public class 파일작성Run {
     public static void main(String[] args) {
         파일작성Service service = new 파일작성Service();
-        service.method1();
+       // service.method1();
+        service.method2();
     }
 }
