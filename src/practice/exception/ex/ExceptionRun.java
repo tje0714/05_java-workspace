fce08a197c28d1ad83ab922451ba4fee7e33bb3d
tje0714 <tje0714@outlook.kr>
2025-09-08 15:09:28 +0900

    IO(입출력)

---
+++ src/practice/exception/ex/ExceptionRun.java
@@ -0,0 +1,12 @@
+package practice.exception.ex;
+
+import edu.exception.pack3.개발자Exception;
+
+public class ExceptionRun {
+    public static void main(String[] args) {
+        ExceptionService es  = new ExceptionService();
+       // es.method1();
+      //  es.method2();
+        es.method4();
+    }
+}
