a90484b0dbc498367c36301c4672b1e232d4d842
tje0714 <tje0714@outlook.kr>
2025-09-09 11:07:01 +0900

    IO(입출력)

---
+++ src/edu/io/pack3/ex/FilePracticeRun.java
@@ -0,0 +1,4 @@
+package edu.io.pack3.ex;
+
+public class FilePracticeRun {
+}
---
+++ src/edu/io/pack3/ex/FilePracticeService.java
@@ -0,0 +1,15 @@
+package edu.io.pack3.ex;
+
+public class FilePracticeService {
+
+    // Path
+    // Files
+
+    //를 이용해서 1. my_logs 라는 폴더가 존재하는지 확인하고
+    // 내부에 intro.txt 파일이 존재하는지 확인
+    // 폴더 / 파일 모두 존재하면
+    // 현대방식의 파일읽기 방식을 활용해서 intro.txt 내부에 작성된 글자를 모두 읽고 읽은 내용 출력
+    public void method1(){
+
+    }
+}
