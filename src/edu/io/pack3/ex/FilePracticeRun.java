7530523540681251379b0637bb454b5db506369d
tje0714 <tje0714@outlook.kr>
2025-09-09 11:48:09 +0900

    IO(입출력)

--- src/edu/io/pack3/ex/FilePracticeRun.java
+++ src/edu/io/pack3/ex/FilePracticeRun.java
@@ -8,8 +8,23 @@ public class FilePracticeRun {
         FilePracticeService fps = new FilePracticeService();
 
         Scanner sc = new Scanner(System.in);
+        System.out.print("폴더를 입력하시겠습니까? (yes/no): ");
+        //              글자입력받고  공백없음 모두소문자처리
+        String choice = sc.nextLine().trim().toLowerCase();
+        String dirName="";
+        if(choice.equals("yes") || choice.equals("y")){
+            System.out.print("폴더명을 입력하세요 : ");
+            dirName = sc.nextLine().trim();
+        } else {
+            dirName = "";
+            System.out.println("기본 프로젝트 최상위 폴더로 설정됩니다.");
+        }
+        // 없으면 기본프로젝트 최상위로 폴더 설정
+        // yes 이면 폴더 입력 후
+        // dirName 으로 입력한 폴더이름 method1 전달
+
         System.out.print("내용을 확인할 파일이름 + 확장자를 입력하세요 : ");
         String fileName = sc.nextLine();
-        fps.method1(fileName);
+        fps.method1(dirName,fileName);
     }
 }
