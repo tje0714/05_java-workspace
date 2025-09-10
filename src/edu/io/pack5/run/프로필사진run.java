327d0b4e4291a3661adf1dafb2d76b2a0e92316a
tje0714 <tje0714@outlook.kr>
2025-09-10 11:14:07 +0900

    IO(입출력) - 프로필 사진 저장하기

--- src/edu/io/pack5/run/프로필사진run.java
+++ src/edu/io/pack5/run/프로필사진run.java
@@ -6,6 +6,17 @@ public class 프로필사진run {
     public static void main(String[] args) {
         프로필사진service service = new 프로필사진service();
 
-        service.createProfileFolder();
+     //   service.createProfileFolder("member03","프로필사진2.png");
+    //    service.createProfileFolder("member04","프로필사진2.png");
+
+        String imgurl = "https://cdn.dailysecu.com/news/photo/202104/123449_145665_1147.png";
+
+      //  service.saveImg(imgurl);
+
+
+        // 인터넷에서 이미지 주소를 복사한 후
+        // 원하는 이미지를
+        String imgUrl2 = "이미지 주소 넣어주기";
+        service.saveImg2(imgUrl2);
     }
 }
