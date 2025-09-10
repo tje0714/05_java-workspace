fedf0b0881769685356e24e5f046eb6a004c2c66
tje0714 <tje0714@outlook.kr>
2025-09-10 13:10:39 +0900

    IO(입출력) - 프로필 사진 저장하기

--- src/edu/io/pack5/run/프로필사진run.java
+++ src/edu/io/pack5/run/프로필사진run.java
@@ -2,6 +2,8 @@ package edu.io.pack5.run;
 
 import edu.io.pack5.service.프로필사진service;
 
+import java.util.Scanner;
+
 public class 프로필사진run {
     public static void main(String[] args) {
         프로필사진service service = new 프로필사진service();
@@ -17,6 +19,22 @@ public class 프로필사진run {
         // 인터넷에서 이미지 주소를 복사한 후
         // 원하는 이미지를
         String imgUrl2 = "https://img.freepik.com/free-vector/cute-shiba-inu-dog-sitting-heaven-with-cloud-cartoon-vector-icon-illustrationanimal-holiday-flat_138676-9999.jpg?semt=ais_hybrid&w=740&q=80";
-        service.saveImg2(imgUrl2);
+        //service.saveImg2(imgUrl2);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("저장할 사진의 주소를 작성하세요 : ");
+        String imgUrl3 = sc.nextLine();
+
+        System.out.println("저장할 사진의 폴더를 작성하세요 : ");
+        String folder = sc.nextLine();
+
+
+        System.out.println("저장할 사진의 명칭을 작성하세요 : ");
+        String name = sc.nextLine();
+
+        String imgName = name + ".png";
+       // service.saveImg3(imgName);
+        service.saveImg4(folder,imgName, imgUrl3);
+
     }
 }
--- src/edu/io/pack5/service/프로필사진service.java
+++ src/edu/io/pack5/service/프로필사진service.java
@@ -99,7 +99,7 @@ public class 프로필사진service {
     }
 
 
-    public void saveImg3(){
+    public void saveImg3(String imgName){
         Path mf = Path.of("profiles", "member07");
 
 
@@ -108,17 +108,67 @@ public class 프로필사진service {
             String imgUrl2 = "https://img.freepik.com/free-vector/holiday-flat_138676-9999.jpg";
 
             Files.createDirectories(mf);
-            Path pf = Path.of("profiles", "member07", "이미지이름.png");
+            Path pf = Path.of("profiles", "member07", imgName);
 
             URL url = new URL(imgUrl2);
             InputStream inputStream = url.openStream();
-
-            Files.copy(inputStream,pf);
+            // 이미지를 복사해서 저장한다는 의미를 쓰는 이유
+            // 웹에 있는 이미지를 그대로 복제해서 내 컴퓨터 안에 저장하는 것이기 때문
+            // 웹에 있는 이미지를 내 컴퓨터 안에 저장한다하여 웹에 존재하는 이미지가 사라지는 것은 아니기에
+            // 웹 이미지를 복제하여 내 컴퓨터에 저장한다  .copy() 를 사용함
+            Files.copy(inputStream,pf); // 똑같은 이름의 이미지가 존재할 때 덮어쓰기가 안됨
 
             inputStream.close();
             System.out.println("이미지 저장 완료되었습니다.");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+
+    // Scanner 를 이용하여 폴더이름 이미지이름 사용자에게 전달받고
+    // 폴더 안에 이미지를 웹에서 복제하여 저장
+    //  확장자는 .png를 사용
+    /*
+    png = 투명도를 지원하는 비손실 압축
+          로고, 아이콘 투명배경이 필요한 이미지 사용
+          파일 크기 큼, 여러번 수정해야할 때 주로 사용
+   jpg  = 투명도를 지원하지 않는 손실 압축
+          사진, 그라데이션, 배너 등 완성된 사진을 압축하여 데이터 크기를 줄여 사용할 때 용이
+   svg  = 확장 가능한 이미지
+          이미지를 아무리 확대하거나 축소해도 절대 깨지지 않는 디지털 이미지 형식
+          수학 공식으로 이미지를 설계
+          화면 크기에 맞춰 매번 공식을 다시 계산 후 표현되기 때문에 , 디바이스에서 유용하게 사용되는 이미지
+          코드 기반 이미지
+     */
+    // 외부 url 을 통해 이미지를 가져올 때 자바에서 접근할 수 없는 주소도 존재함
+    // 사이트마다 권한에 따른 문제
+    //https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Oryctolagus_cuniculus_Tasmania_2.jpg/960px-Oryctolagus_cuniculus_Tasmania_2.jpg
+    // 주소에 접근하려면 추가적인 권한이 필요하여 권한이 필요하다는 예외가 발생
+
+
+    // Scanner 로 원하는 이미지 주소를 넣었을 때 권한 문제 없이 정상적으로 작동
+    //https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTZWlPjghR3F6rno2RtA56T9mRyUL_BWILiAmpxP0-npm6nmw-Gsm9AFDYLAl8paow4CEKMeRHaQn39tB4VniZan8svg2JIpXXOO6L84F4
+    public void saveImg4(String folder,String imgName,String imgUrl3){
+        Path pf = Path.of("profiles", folder, imgName);
+
+        try {
+            // 폴더가 없을 경우 생성 있으면 false 넘어가기
+            Files.createDirectories(pf.getParent());
+
+            // 폴더 안에 이미지까지 바라보는 경로
+           // String imgUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj1Q9ptlvl37Fzv9v-CP7f0VQ_Q8czmbf9ug&s";
+            URL url = new URL(imgUrl3);
+            InputStream inputStream = url.openStream();
+            Files.copy(inputStream,pf);
+            System.out.println("이미지 저장을 완료했습니다.");
+        } catch (IOException e) {
+            System.out.println("이미지 다운로드 실패");
+            // 더 자세한 오류 정보 제공
+            if (e.getMessage().contains("403")) {
+                System.out.println("403 오류 : 위키디아 에서 접근을 거부했습니다. 함부로 들어와서 다운로드 할 수 없다네요..");
+            }
+        }
+
     }
 }
